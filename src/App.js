5abaa5d17dc6c5ebd5208a3bca1907125a8bc665
AndreasFolomkin <66010938+AndreasFolomkin@users.noreply.github.com>
2021-03-20 15:54:58 +0200

    add public/

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import './App.css';
-import axios from "axios";
+import axios from 'axios';
 import TableCustom from "./Components/Table/Table";
 import Upload from "./Components/Upload/Upload";
 
